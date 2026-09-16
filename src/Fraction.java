55e880f920f936d5bcaa3b66d628355e264b07a1
foudfoud <benelmadjatf@gmail.com>
2026-09-16 15:02:31 +0200

    Ajout des attributs, constructeurs et toString

---
+++ src/Fraction.java
@@ -0,0 +1,20 @@
+public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;}
+
+    public Fraction(int numerator) {
+        this.numerator = numerator;
+        this.denominator = 1;}
+
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;}
+
+    @Override
+    public String toString() {
+        return "Je suis une fraction.";}}
